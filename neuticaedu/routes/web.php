7b36745fd384f2c78b4b49704f566838633bd047
dafwann <daffadermawan3@gmail.com>
2025-11-29 07:40:06 +0800

    Update

--- neuticaedu/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-
-
-Route::get('/{any}', [PageController::class, 'index'])
-    ->where('any', '^(?!api).*$');
